0b411987976a3ac98b2f0a3091cadf9468c085b6
yakls <yakls@mail.ru>
2021-10-02 22:21:14 -0400

    dfd

---
+++ testOne/src/main/java/MyTestProject/testOne/App.java
@@ -0,0 +1,16 @@
+package MyTestProject.testOne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+  
+        
+    }
+}
